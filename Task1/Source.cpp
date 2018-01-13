12d458d3b0b4704e7de56186fbf785e8925ad82e
LitvinovKV <SquertelSquertel@mail.ru>
2018-01-13 14:06:09 +0300

    Waterlemon Task
    В один из жарких летних дней Петя и его друг Вася решили купить арбуз. Они выбрали самый большой и самый спелый, на их взгляд. После недолгой процедуры взвешивания весы показали w килограмм. Поспешно прибежав домой, изнемогая от жажды, ребята начали делить приобретенную ягоду, однако перед ними встала нелегкая задача. Петя и Вася являются большими поклонниками четных чисел, поэтому хотят поделить арбуз так, чтобы доля каждого весила именно четное число килограмм, при этом не обязательно, чтобы доли были равными по величине. Ребята очень сильно устали и хотят скорее приступить к трапезе, поэтому Вы должны подсказать им, удастся ли поделить арбуз, учитывая их пожелание. Разумеется, каждому должен достаться кусок положительного веса.
    Входные данные
    
    В первой и единственной строке входных данных записано целое число w (1 ≤ w ≤ 100) — вес купленного ребятами арбуза.
    Выходные данные
    
    Выведите YES, если ребята смогут поделить арбуз на две части, каждая из которых весит четное число килограмм, и NO в противном случае.

---
+++ Task1/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+bool ifEven(int& val) {
+	bool local = val & 1;
+	return local;
+}
+
+int main() {
+
+	int x = 0;
+	cout << "Enter weight watermelon: "; cin >> x;
+	else
+		cout << "YES" << endl;
+
+	system("pause");
+	return 0;
+}
